f53a7911c169bf1c16c933134103e657b508c639
WeddingsInvitations <anangrifky31@gmail.com>
2025-05-02 15:53:14 +0700

    Update home.js

--- home.js
+++ home.js
@@ -1,3 +1,10 @@
+// Animasi logo container//
+window.onload = function () {
+  setTimeout(() => {
+    document.querySelector('.logo-container').classList.add('animate');
+  }, 500); // Delay 0.5 detik
+};
+
 // Animasi foto pasangan muncul dari atas saat halaman dimuat
 window.addEventListener('DOMContentLoaded', () => {
   const photo = document.querySelector('.couple-photo');
